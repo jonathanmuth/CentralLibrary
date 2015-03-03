59a32a414e95dad5b9e7e5dff527a53ca5cc108b
Jonathan Muth <contact@jonathanmuth.com>
2015-03-03 23:11:31 +0100

    Implement basic login

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,28 +1,37 @@
 <nav role="navigation"> 
 
-  <!-- 
-
-  --><ul class="menu cf">
-    <li>
-      <a <?php e($page->isHomePage(), ' class="active"') ?> href="<?php echo $site->url() ?>">Library</a>
-      <a class="action" href="http://127.0.0.1:8000/download"><i class="fa fa-download"></i></a>
-    </li>
-    <li>
-      <a <?php e(($page->uid() == 'references'), ' class="active"') ?> href="<?php echo $site->url() ?>/references">References</a>
-      <a class="action" href="http://127.0.0.1:8000/panel/#/pages/add/references"><i class="fa fa-plus-circle"></i></a>
-    </li>
-    <li>
-      <a <?php e(($page->uid() == 'collections'), ' class="active"') ?> href="<?php echo $site->url() ?>/collections">Collections</a>
-    </li>
-
-  </ul><!-- 
-
-   --><div class="search">
+  <div class="search">
 
     <form action="<?php echo url('/references/') ?>">
       <input class="search__input" type="search" name="q" value="<?php echo $query ?>" autofocus />
     </form>
 
   </div>
+
+  <ul class="menu cf">
+    <li <?php e($page->isHomePage(), ' class="active"') ?>>
+      <a href="<?php echo $site->url() ?>">Library</a>
+    </li><!-- 
+     --><li <?php e(($page->uid() == 'references') or ($page->isChildOf($pages->find('references'))), ' class="active"') ?>>
+      <a href="<?php echo $site->url() ?>/references">References</a>
+    </li><!-- 
+     --><li <?php e(($page->uid() == 'collections'), ' class="active"') ?>>
+      <a href="<?php echo $site->url() ?>/collections">Collections</a>
+    </li><!-- 
+     --><li>
+      <a class="action" href="http://127.0.0.1:8000/download"><i class="fa fa-download"></i></a>
+    </li><!-- 
+     --><li>
+      <a class="action" href="http://127.0.0.1:8000/panel/#/pages/add/references"><i class="fa fa-plus-circle"></i></a>
+    </li><!-- 
+     --><li <?php e(($page->uid() == 'login'), ' class="active"') ?>>
+      <?php if($user = $site->user()): ?>
+          <a href="<?php echo url('logout') ?>">Sign Out</a>
+      <?php else: ?>
+          <a class="action" href="http://127.0.0.1:8000/login">Sign In</a>
+      <?php endif ?>
+    </li>
+
+  </ul>
 
 </nav>
